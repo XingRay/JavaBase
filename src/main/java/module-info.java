38f58a61d6cf0db716e507fa68d6d43c17e09c17
leixing <leixing1012@qq.com>
2021-02-10 21:45:43 +0800

    开放Result类访问权限

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,4 +2,6 @@ module JavaBase {
     exports com.xingray.javabase.interfaces;
     exports com.xingray.javabase.result;
     exports com.xingray.javabase.range;
+
+    opens com.xingray.javabase.result;
 }
